b72713cbc566ff2411b580a9f182ee26e97443cf
amrubic23 <71724868+amrubic23@users.noreply.github.com>
2020-09-28 12:43:27 -0700

    Sample Test Cases

---
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        string test_url = "http://192.168.0.26:8080/login";
+
+        IWebDriver driver;
+        [SetUp]
+        public void start_browser()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test_user()
+        {
+            driver.Url = test_url;
+
+            //System.Threading.Thread.Sleep(2000);
+
+            IWebElement loginName = driver.FindElement(By.Name("User"));
+            loginName.SendKeys("adam");
+
+            IWebElement passName = driver.FindElement(By.Name("Pass"));
+            passName.SendKeys("1234");
+
+            IWebElement loginButton = driver.FindElement(By.Id("btn1"));
+            loginButton.Click();
+
+            //System.Threading.Thread.Sleep(5000);
+
+            Console.WriteLine("Test Passed");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ NUnitTestProject1/NUnitTestProject1/UnitTest2.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace NUnitTestProject1
+{
+    public class UnitTest2
+    {
+        string test_url = "http://192.168.0.26:8080/login";
+
+        IWebDriver driver;
+        [SetUp]
+        public void start_browser()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test_admin()
+        {
+            driver.Url = test_url;
+
+            //System.Threading.Thread.Sleep(2000);
+
+            IWebElement loginName = driver.FindElement(By.Name("User"));
+            loginName.SendKeys("admin");
+
+            IWebElement passName = driver.FindElement(By.Name("Pass"));
+            passName.SendKeys("admin");
+
+            IWebElement loginButton = driver.FindElement(By.Id("btn1"));
+            loginButton.Click();
+
+            //System.Threading.Thread.Sleep(5000);
+
+            Console.WriteLine("Test Passed");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
+        }
+    }
+}
