703c3635beaf97921baa6550b814e486a4ed69de
amrubic23 <71724868+amrubic23@users.noreply.github.com>
2020-10-02 12:08:26 -0700

    og cert test again

--- WebApplication15/Program.cs
+++ WebApplication15/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,14 +15,25 @@ namespace WebApplication15
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        /*public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });*/
+       public static IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .ConfigureKestrel(options =>
+            {
+                options.ConfigureHttpsDefaults(opt =>
+                    opt.ClientCertificateMode =
+                        ClientCertificateMode.RequireCertificate);
+            })
+            .Build();
     }
 }
