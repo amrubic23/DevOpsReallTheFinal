0b72bcacb26bac9a09d60f880ea24c8674928e22
amrubic23 <71724868+amrubic23@users.noreply.github.com>
2020-10-13 17:56:54 -0700

    added update button

--- WebApplication15/Pages/Update.cshtml.cs
+++ WebApplication15/Pages/Update.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication15.Pages
-{
+{//a
     public class UpdateModel : PageModel
     {
         public string Pass => (string)TempData[nameof(Pass)];
